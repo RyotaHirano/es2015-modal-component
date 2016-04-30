e208b146031a7d1478c1a7e3ace64b5ec32d0d84
Ryota Hirano <hirano@tsumikiinc.com>
2016-05-01 03:27:13 +0900

    Add Inner Scroll Modal Demo.

--- demo/src/js/index.js
+++ demo/src/js/index.js
@@ -30,5 +30,33 @@ const initModal = () => {
       modal.render($(img));
     });
   });
+
+  // Modal Resized
+  $('.js-modal-open--resize').each((i, el) => {
+    return new Modal(el, {
+      modalScrollSelector: '.js-modal-resize-inner',
+      isResizeModalHeight: true,
+      modalHeightRatio: 0.6
+    }).on('open', function(el, modal) {
+      // Create Modal Contents
+      const modalContents = document.createElement('div');
+      modalContents.classList.add('c-modal', 'js-modal-resize-inner');
+      const modalContentsParagraph = document.createElement('p');
+      modalContentsParagraph.classList.add('c-text', 'c-text--l', 'c-text-w--b');
+      modalContentsParagraph.textContent =
+        'This is Modal Contents.This is Modal Contents.This is Modal Contents.' +
+        'This is Modal Contents.This is Modal Contents.This is Modal Contents.' +
+        'This is Modal Contents.This is Modal Contents.This is Modal Contents.' +
+        'This is Modal Contents.This is Modal Contents.This is Modal Contents.' +
+        'This is Modal Contents.This is Modal Contents.This is Modal Contents.' +
+        'This is Modal Contents.This is Modal Contents.This is Modal Contents.' +
+        'This is Modal Contents.This is Modal Contents.This is Modal Contents.' +
+        'This is Modal Contents.This is Modal Contents.This is Modal Contents.' +
+        'This is Modal Contents.This is Modal Contents.This is Modal Contents.';
+      modalContents.appendChild(modalContentsParagraph);
+      // Modal render
+      modal.render(modalContents);
+    });
+  });
 }
 initModal();
